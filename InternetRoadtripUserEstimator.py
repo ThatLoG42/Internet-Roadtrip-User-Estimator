fe1db8b2db5b27459de0b11b7541dbaebca30afd
ThatLoG42 <log42lol@gmail.com>
2025-06-06 19:38:14 -0700

    Update InternetRoadtripUserEstimator.py
    made turnout percentages a tuple for conciseness and added type hints

--- InternetRoadtripUserEstimator.py
+++ InternetRoadtripUserEstimator.py
@@ -3,36 +3,31 @@ import json
 import statistics
 # import threading
 
-def main():
+def main() -> None:
     readableOE()
     # set_interval(readableOE,30)
 
-def onlineEstimate():
+def onlineEstimate() -> tuple[float,float]:
     info = requests.get("https://roadtrip.pikarocks.dev/queryTime?limit=20")
-    estimates = []
+    estimates: list[float] = []
     stopJson = json.loads(info.text)
     reportedUserCount = stopJson["results"][0]["totalUsers"]
     print(f"Reported User Count: {reportedUserCount} drivers online")
     for stop in stopJson["results"]:
-        voteResults = json.loads(stop["voteCounts"])
-        voteCount = stop["voteCount"]
+        voteResults: dict = json.loads(stop["voteCounts"])
+        voteCount: int = stop["voteCount"]
         if (len(voteResults) > 3):
-            high = voteCount * 100/3
-            mid = voteCount * 100/5
-            eight = voteCount * 100/8
-            low = voteCount * 100/10
-            estimates += [high,mid,eight,low]
+            turnoutPercents = [3,5,8,10]
+            for turnoutPercent in turnoutPercents:
+                estimates.append(voteCount*100/turnoutPercent)
         else:
-            two = voteCount * 100/2
-            high = voteCount * 100/3
-            mid = voteCount * 100/5
-            low = voteCount * 100/7
-            nine = voteCount * 100/9
-            estimates += [two,low,mid,high,nine]  
+            turnoutPercents = [2,3,5,7,9]
+            for turnoutPercent in turnoutPercents:
+                estimates.append(voteCount*100/turnoutPercent) 
     return (statistics.mean(estimates),statistics.stdev(estimates))
 
 
-def readableOE():
+def readableOE() -> None:
     estimate = onlineEstimate()
     print(f"LoG42's Actual Online Estimate (based on vote counts): {round(estimate[0],5)} ± {round(estimate[1],5)} drivers online\n")
 
