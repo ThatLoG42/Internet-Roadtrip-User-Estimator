5870a2248b11ba544c3c60cb6de270ca20e36c50
ThatLoG42 <log42lol@gmail.com>
2025-06-07 15:57:42 -0700

    Add mean to estimator
    added separate mean and median function, as well as having the request be passed in through a param in readableOE and onlineEstimate

--- InternetRoadtripUserEstimator.py
+++ InternetRoadtripUserEstimator.py
@@ -2,18 +2,19 @@ import requests
 import json
 import statistics
 from scipy import stats
+from typing import Literal
 # import threading
 
 def main() -> None:
-    readableOE()
+    info: requests.Response = requests.get("https://roadtrip.pikarocks.dev/queryTime?limit=20")
+    reportedUserCount = json.loads(info.text)["results"][0]["totalUsers"]
+    print(f"Reported User Count: {reportedUserCount} drivers online")
+    readableOE(info)
     # set_interval(readableOE,30)
 
-def onlineEstimate() -> tuple[float,float]:
-    info = requests.get("https://roadtrip.pikarocks.dev/queryTime?limit=20")
+def onlineEstimate(info: requests.Response, type: Literal["mean","median"]) -> tuple[float,float]:
     estimates: list[float] = []
     stopJson = json.loads(info.text)
-    reportedUserCount = stopJson["results"][0]["totalUsers"]
-    print(f"Reported User Count: {reportedUserCount} drivers online")
     for stop in stopJson["results"]:
         voteResults: dict = json.loads(stop["voteCounts"])
         voteCount: int = stop["voteCount"]
@@ -25,12 +26,18 @@ def onlineEstimate() -> tuple[float,float]:
             turnoutPercents = (2,3,5,7,9,12)
             for turnoutPercent in turnoutPercents:
                 estimates.append(voteCount*100/turnoutPercent) 
-    return (statistics.median(estimates),stats.median_abs_deviation(estimates))
+    if type == "median":
+        return (statistics.median(estimates),stats.median_abs_deviation(estimates))
+    elif type == "mean":
+        return (statistics.mean(estimates),statistics.stdev(estimates))
+    else:
+        raise TypeError("Input must be either 'mean' or 'median'")
 
 
-def readableOE() -> None:
-    estimate = onlineEstimate()
-    print(f"LoG42's Actual Online Estimate (based on vote counts): {round(estimate[0],5)} ± {round(estimate[1],5)} drivers online\n")
+def readableOE(info: requests.Response) -> None:
+    estimates = {"Mean": onlineEstimate(info, "mean"), "Median": onlineEstimate(info, "median")}
+    for label, estimate in estimates.items():
+        print(f"LoG42's {label}-based Actual Online Estimate (based on vote counts): {round(estimate[0],5)} ± {round(estimate[1],5)} drivers online\n")
 
 
 # def set_interval(func, sec):
